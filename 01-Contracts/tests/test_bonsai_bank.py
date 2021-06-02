1c5b2356ff151db48e8bd541482edf3319a80325
Michael Ghen <mike@mikeghen.com>
2021-06-02 14:32:53 -0400

    uncomment that block

--- 01-Contracts/tests/test_bonsai_bank.py
+++ 01-Contracts/tests/test_bonsai_bank.py
@@ -108,121 +108,120 @@ def test_mint_bonsai():
     assert 0 == bbank.waterBalance(bonsai_id)
     assert 0 == bbank.fertilizerBalance(bonsai_id)
     assert 0 == bbank.lifeStage(bonsai_id)
-#
-# # bonsai can only be minted by the botanist
-# def test_mint_not_botanist(bbank, dai, weth, caretaker, botanist):
-#     # Check that a new minted bonsai is created with all the right properties
-#     bonsai_uri = "ipfs://aaaa"
-#     # Mint a new token as the caretaker and revert
-#     with brownie.reverts():
-#         bonsai_id = bbank.mint(caretaker, bonsai_uri, {"from": caretaker})
-#
-# # bonsai can be watered
-# def test_watering(bbank, dai, weth, caretaker, botanist):
-#     bonsai_uri = "ipfs://aaaa"
-#     bonsai_id = bbank.mint(caretaker, bonsai_uri, {"from": botanist})
-#     water_rate = bbank.waterRate()
-#     dai.approve(bbank.address, water_rate)
-#     starting_dai = dai.balanceOf(caretaker)
-#     bbank.water(bonsai_id, {"from": caretaker})
-#     ending_dai = dai.balanceOf(caretaker)
-#     assert starting_dai - ending_dai == water_rate
-#     assert 0 != bbank.lastWatered(bonsai_id)
-#     assert 1 == bbank.consecutiveWaterings(bonsai_id)
-#     assert 0 == bbank.waterBalance(bonsai_id)
-#     assert 0 == bbank.fertilizerBalance(bonsai_id)
-#
-# # bonsai can only be watered every 7 days
-# def test_watering_early(bbank, dai, weth, caretaker, botanist):
-#     bonsai_uri = "ipfs://aaaa"
-#     bonsai_id = bbank.mint(caretaker, bonsai_uri, {"from": botanist})
-#     water_rate = bbank.waterRate()
-#     dai.approve(bbank.address, water_rate)
-#     bbank.water(bonsai_id, {"from": caretaker})
-#     # Second watering will revert bc it hasn't been 7 days
-#     with brownie.reverts():
-#         bbank.water(bonsai_id, {"from": caretaker})
-#
-# # bonsai can be fertilized
-# def test_fertilizing(bbank, dai, weth, caretaker, botanist):
-#     bonsai_uri = "ipfs://aaaa"
-#     bonsai_id = bbank.mint(caretaker, bonsai_uri, {"from": botanist})
-#     fert_rate = bbank.fertRate()
-#     weth.approve(bbank.address, fert_rate)
-#     starting_weth = weth.balanceOf(caretaker)
-#     bbank.fertilize(bonsai_id, {"from": caretaker})
-#     ending_weth = weth.balanceOf(caretaker)
-#     assert starting_weth - ending_weth == fert_rate
-#     assert 0 != bbank.lastFertilized(bonsai_id)
-#     assert 1 == bbank.consecutiveFertilizings(bonsai_id)
-#     assert 0 == bbank.waterBalance(bonsai_id)
-#     assert 0 == bbank.fertilizerBalance(bonsai_id)
-#
-# # bonsai can only be fertilized every 30 days
-# def test_fertilizing_early(bbank, dai, weth, caretaker, botanist):
-#     bonsai_uri = "ipfs://aaaa"
-#     bonsai_id = bbank.mint(caretaker, bonsai_uri, {"from": botanist})
-#     fert_rate = bbank.fertRate()
-#     dai.approve(bbank.address, water_rate)
-#     bbank.fertilize(bonsai, {"from": caretaker})
-#     # Second fertilizing will revert bc. it hasn't been 30 days
-#     with brownie.reverts():
-#         bbank.fertilize(bonsai, {"from": caretaker})
-#
-# # bondai can grow
-# def test_grow(bbank, dai, weth, caretaker, botanist, bonsai_grow):
-#     new_bonsai_uri = "ipfs://nnnn"
-#     bbank.grow(bonsai_grow, new_bonsai_uri, {"from": botanist})
-#     ending_weth = weth.balanceOf(caretaker)
-#     # Resets params
-#     assert 0 == bbank.consecutiveWaterings(bonsai_id)
-#     assert 0 == bbank.consecutiveFertilizings(bonsai_id)
-#     assert ["ipfs://gggg", new_bonsai_uri] == bbank.bonsaiUris(bonsai_id)
-#     assert 1 == bbank.lifeStage(bonsai_id)
-#
-# # bonsai can only grow after 90 days of tending
-# def test_grow_early(bbank, dai, weth, caretaker, botanist, bonsai_grow):
-#     new_bonsai_uri = "ipfs://nnnn"
-#     bbank.grow(bonsai_grow, new_bonsai_uri, {"from": botanist})
-#     # Second fertilizing will revert bc. it hasn't been 30 days
-#     with brownie.reverts():
-#         bbank.grow(bonsai_grow, new_bonsai_uri, {"from": botanist})
-#
-#
-# # bonsai can wither
-# def test_wither(bbank, dai, weth, caretaker, botanist, bonsai_wither):
-#     nft_dai = bbank.waterBalance(bonsai_id)
-#     nft_weth = bbank.fertilizerBalance(bonsai_id)
-#     bot_dai_bal_starting = dai.balanceOf(botanist)
-#     bot_weth_bal_starting = weth.balanceOf(botanist)
-#     bbank.wither(bonsai_wither, {"from": botanist})
-#     ending_weth = weth.balanceOf(caretaker)
-#     bot_dai_bal_ending = dai.balanceOf(botanist)
-#     bot_weth_bal_ending = weth.balanceOf(botanist)
-#     assert bot_dai_bal_starting - bot_dai_bal_ending == nft_dai * 0.95
-#     assert bot_weth_bal_starting - bot_weth_bal_ending == nft_weth * 0.95
-#     assert 0 == bbank.consecutiveWaterings(bonsai_id)
-#     assert 0 == bbank.consecutiveFertilizings(bonsai_id)
-#
-#
-# # bonsai can only wither after 14 days without water
-# def test_wither_early(bbank, dai, weth, caretaker, botanist, bonsai_wither):
-#     bbank.wither(bonsai_wither, {"from": botanist})
-#     # Second fertilizing will revert bc. it hasn't been 30 days
-#     with brownie.reverts():
-#         bbank.wither(bonsai_wither, {"from": botanist})
-#
-# # bonsai can be destroyed to recover the funds
-# def test_destroy(bbank, dai, weth, caretaker, botanist, bonsai_wither):
-#     bonsai_dai = bbank.waterBalance(bonsai_id)
-#     bonsai_weth = bbank.fertilizerBalance(bonsai_id)
-#     dai_bal_starting = dai.balanceOf(botanist)
-#     weth_bal_starting = weth.balanceOf(botanist)
-#     bbank.destroy(bonsai_grow, {"from":caretaker})
-#     dai_bal_ending = dai.balanceOf(botanist)
-#     weth_bal_ending = weth.balanceOf(botanist)
-#     assert dai_bal_starting - dai_bal_ending == bonsai_dai
-#     assert weth_bal_starting - weth_bal_ending == bonsai_weth
-#     assert 0 == bbank.waterBalance(bonsai_id)
-#     assert 0 == bbank.fertilizerBalance(bonsai_id)
-#
+
+# bonsai can only be minted by the botanist
+def test_mint_not_botanist(bbank, dai, weth, caretaker, botanist):
+    # Check that a new minted bonsai is created with all the right properties
+    bonsai_uri = "ipfs://aaaa"
+    # Mint a new token as the caretaker and revert
+    with brownie.reverts():
+        bonsai_id = bbank.mint(caretaker, bonsai_uri, {"from": caretaker})
+
+# bonsai can be watered
+def test_watering(bbank, dai, weth, caretaker, botanist):
+    bonsai_uri = "ipfs://aaaa"
+    bonsai_id = bbank.mint(caretaker, bonsai_uri, {"from": botanist})
+    water_rate = bbank.waterRate()
+    dai.approve(bbank.address, water_rate)
+    starting_dai = dai.balanceOf(caretaker)
+    bbank.water(bonsai_id, {"from": caretaker})
+    ending_dai = dai.balanceOf(caretaker)
+    assert starting_dai - ending_dai == water_rate
+    assert 0 != bbank.lastWatered(bonsai_id)
+    assert 1 == bbank.consecutiveWaterings(bonsai_id)
+    assert 0 == bbank.waterBalance(bonsai_id)
+    assert 0 == bbank.fertilizerBalance(bonsai_id)
+
+# bonsai can only be watered every 7 days
+def test_watering_early(bbank, dai, weth, caretaker, botanist):
+    bonsai_uri = "ipfs://aaaa"
+    bonsai_id = bbank.mint(caretaker, bonsai_uri, {"from": botanist})
+    water_rate = bbank.waterRate()
+    dai.approve(bbank.address, water_rate)
+    bbank.water(bonsai_id, {"from": caretaker})
+    # Second watering will revert bc it hasn't been 7 days
+    with brownie.reverts():
+        bbank.water(bonsai_id, {"from": caretaker})
+
+# bonsai can be fertilized
+def test_fertilizing(bbank, dai, weth, caretaker, botanist):
+    bonsai_uri = "ipfs://aaaa"
+    bonsai_id = bbank.mint(caretaker, bonsai_uri, {"from": botanist})
+    fert_rate = bbank.fertRate()
+    weth.approve(bbank.address, fert_rate)
+    starting_weth = weth.balanceOf(caretaker)
+    bbank.fertilize(bonsai_id, {"from": caretaker})
+    ending_weth = weth.balanceOf(caretaker)
+    assert starting_weth - ending_weth == fert_rate
+    assert 0 != bbank.lastFertilized(bonsai_id)
+    assert 1 == bbank.consecutiveFertilizings(bonsai_id)
+    assert 0 == bbank.waterBalance(bonsai_id)
+    assert 0 == bbank.fertilizerBalance(bonsai_id)
+
+# bonsai can only be fertilized every 30 days
+def test_fertilizing_early(bbank, dai, weth, caretaker, botanist):
+    bonsai_uri = "ipfs://aaaa"
+    bonsai_id = bbank.mint(caretaker, bonsai_uri, {"from": botanist})
+    fert_rate = bbank.fertRate()
+    dai.approve(bbank.address, water_rate)
+    bbank.fertilize(bonsai, {"from": caretaker})
+    # Second fertilizing will revert bc. it hasn't been 30 days
+    with brownie.reverts():
+        bbank.fertilize(bonsai, {"from": caretaker})
+
+# bondai can grow
+def test_grow(bbank, dai, weth, caretaker, botanist, bonsai_grow):
+    new_bonsai_uri = "ipfs://nnnn"
+    bbank.grow(bonsai_grow, new_bonsai_uri, {"from": botanist})
+    ending_weth = weth.balanceOf(caretaker)
+    # Resets params
+    assert 0 == bbank.consecutiveWaterings(bonsai_id)
+    assert 0 == bbank.consecutiveFertilizings(bonsai_id)
+    assert ["ipfs://gggg", new_bonsai_uri] == bbank.bonsaiUris(bonsai_id)
+    assert 1 == bbank.lifeStage(bonsai_id)
+
+# bonsai can only grow after 90 days of tending
+def test_grow_early(bbank, dai, weth, caretaker, botanist, bonsai_grow):
+    new_bonsai_uri = "ipfs://nnnn"
+    bbank.grow(bonsai_grow, new_bonsai_uri, {"from": botanist})
+    # Second fertilizing will revert bc. it hasn't been 30 days
+    with brownie.reverts():
+        bbank.grow(bonsai_grow, new_bonsai_uri, {"from": botanist})
+
+
+# bonsai can wither
+def test_wither(bbank, dai, weth, caretaker, botanist, bonsai_wither):
+    nft_dai = bbank.waterBalance(bonsai_id)
+    nft_weth = bbank.fertilizerBalance(bonsai_id)
+    bot_dai_bal_starting = dai.balanceOf(botanist)
+    bot_weth_bal_starting = weth.balanceOf(botanist)
+    bbank.wither(bonsai_wither, {"from": botanist})
+    ending_weth = weth.balanceOf(caretaker)
+    bot_dai_bal_ending = dai.balanceOf(botanist)
+    bot_weth_bal_ending = weth.balanceOf(botanist)
+    assert bot_dai_bal_starting - bot_dai_bal_ending == nft_dai * 0.95
+    assert bot_weth_bal_starting - bot_weth_bal_ending == nft_weth * 0.95
+    assert 0 == bbank.consecutiveWaterings(bonsai_id)
+    assert 0 == bbank.consecutiveFertilizings(bonsai_id)
+
+
+# bonsai can only wither after 14 days without water
+def test_wither_early(bbank, dai, weth, caretaker, botanist, bonsai_wither):
+    bbank.wither(bonsai_wither, {"from": botanist})
+    # Second fertilizing will revert bc. it hasn't been 30 days
+    with brownie.reverts():
+        bbank.wither(bonsai_wither, {"from": botanist})
+
+# bonsai can be destroyed to recover the funds
+def test_destroy(bbank, dai, weth, caretaker, botanist, bonsai_wither):
+    bonsai_dai = bbank.waterBalance(bonsai_id)
+    bonsai_weth = bbank.fertilizerBalance(bonsai_id)
+    dai_bal_starting = dai.balanceOf(botanist)
+    weth_bal_starting = weth.balanceOf(botanist)
+    bbank.destroy(bonsai_grow, {"from":caretaker})
+    dai_bal_ending = dai.balanceOf(botanist)
+    weth_bal_ending = weth.balanceOf(botanist)
+    assert dai_bal_starting - dai_bal_ending == bonsai_dai
+    assert weth_bal_starting - weth_bal_ending == bonsai_weth
+    assert 0 == bbank.waterBalance(bonsai_id)
+    assert 0 == bbank.fertilizerBalance(bonsai_id)
